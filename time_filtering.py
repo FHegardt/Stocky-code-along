da5fc3170847f4a6fc50311afd13722da71ddef2
FHegardt <39218412+FHegardt@users.noreply.github.com>
2021-11-11 10:51:08 +0100

    saved working

--- time_filtering.py
+++ time_filtering.py
@@ -1,11 +1,8 @@
-from datetime import date, datetime
 from dateutil.relativedelta import relativedelta
+from datetime import datetime
 
-now = datetime.now()
-yesterday = now.date() - relativedelta(days = 1)
-print(yesterday)
-
-def filtertime(df, days = 0):
+def filter_time(df, days = 0):
     last_day = df.index[0].date()
-    start_day = last_day - relativedelta(days = days)
+    start_day = last_day-relativedelta(days = days)
     df = df.sort_index().loc[start_day:last_day]
+    return df 
